0b15ffbdd66d846a4723998409dedbca72e57094
sunnyday0206 <sunme8412@gmail.com>
2026-03-31 09:53:31 +0900

    init: initial project setup

---
+++ src/main/java/org/example/bumditbul_be/BumditbulBeApplication.java
@@ -0,0 +1,13 @@
+package org.example.bumditbul_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BumditbulBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BumditbulBeApplication.class, args);
+    }
+
+}
